3698562197b2c1cedd8e3ce94cc01146174ebe7d
slamaslam466 <slamaslam466@gmail.com>
2021-02-12 00:49:16 +0530

    webinar

--- src/components/class.js
+++ src/components/class.js
@@ -23,8 +23,9 @@ class Class extends Component {
     <div class="panel-heading active" role="tab" id="headingOne">
       <h4 class="panel-title">
         <a  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-         <span>Webinar On Full Stack Development</span><span class="size"style={{marginLeft: 242}}>15/2/2021</span> <span style={{paddingLeft:70}}><button  type="button" class="click">Register Now</button></span><br/>
-         <span class="presenter">by Aslam Ceo,Mytech services Alaska</span><span class="size" style={{marginLeft: 290}}>03.00pm</span>
+         <span>Webinar On Full Stack Development</span><span class="size"style={{marginLeft: 242}}>15/2/2021</span> <br/>
+         <span class="presenter">by Aslam Ceo,Mytech services Alaska</span><span class="size" style={{marginLeft: 277}}>03.00 PM</span>
+          <div style={{paddingLeft:700,marginTop:-30}}><button  type="button" class="click">Register Now</button></div>
         </a>
 
       </h4>
@@ -34,28 +35,30 @@ class Class extends Component {
  
 <div class="row">
     
-    <div class="col-sm-6">
+    <div class="col-sm-6" style={{
+    paddingLeft: 17
+}}>
         <div class="middle">
      <p class="top">Description</p>
      <p class="alignment">Get a deeper understanding of Client Side Programming or in other words the front end development by developing highly responsive web pages across languages.A full stack web developer is a person who can develop both client and server software. In addition to mastering HTML and CSS, he/she also knows how to: Program a browser (like using JavaScript, jQuery, Angular, or Vue) Program a server (like using PHP, ASP, Python, or Node)</p>
       <p class="top">Key Takeaways from Webinar</p>
-     <span><ol style={{width:360,textAlign:'left'}}><li>What is Full Stack Development</li>
+     <span><ol class="alignment" style={{width:360}}><li>What is Full Stack Development</li>
      <li>What is Full Stack Development</li>
      <li>Opportunities in full stack</li>
      <li>Future Scope</li>
      <li>Nuances of Full stack Development</li></ol></span>
     </div>
     </div>
-    
+    <div class="alignment">
     <div class="col-sm-6"style={{    paddingLeft: 0
 }} ><div><img class="topnav2" src="guvi.png"/></div>
       <p class="alignment">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.   </p>
     </div>
     <div class="col-sm-6" >
-     <span style={{float:"left"}}>A.Mohamed Aslam</span><br/>
-       <span style={{float:"left"}}>Ceo,Mytech services Alaska</span>  
+    <p class="alignment1"style={{float:"left",color:'black',marginBottom:0}}>A.Mohamed Aslam</p><br/>
+       <span style={{float:"left",fontSize:12}}>Ceo,Mytech services Alaska</span>  
     </div>
-  </div>
+  </div></div>
          
       </div>
     </div>
@@ -64,8 +67,9 @@ class Class extends Component {
     <div class="panel-heading" role="tab" id="headingTwo">
       <h4 class="panel-title">
         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          <span>Webinar On Full Stack Development</span><span class="size"style={{marginLeft: 242}}>15/2/2021</span> <span style={{paddingLeft:70}}><button  type="button" class="click">Register Now</button></span><br/>
-         <span class="presenter">by Aslam Ceo,Mytech services Alaska</span><span class="size" style={{marginLeft: 290}}>03.00pm</span>
+          <span>Webinar On Full Stack Development</span><span class="size"style={{marginLeft: 242}}>15/2/2021</span> <br/>
+         <span class="presenter">by Aslam Ceo,Mytech services Alaska</span><span class="size" style={{marginLeft: 277}}>03.00 PM</span>
+          <div style={{paddingLeft:700,marginTop:-30}}><button  type="button" class="click">Register Now</button></div>
         </a>
       </h4>
     </div>
@@ -74,12 +78,14 @@ class Class extends Component {
        
 <div class="row">
     
-    <div class="col-sm-6">
+    <div class="col-sm-6" style={{
+    paddingLeft: 17
+}}>
         <div class="middle">
      <p class="top">Description</p>
      <p class="alignment">Get a deeper understanding of Client Side Programming or in other words the front end development by developing highly responsive web pages across languages.A full stack web developer is a person who can develop both client and server software. In addition to mastering HTML and CSS, he/she also knows how to: Program a browser (like using JavaScript, jQuery, Angular, or Vue) Program a server (like using PHP, ASP, Python, or Node)</p>
       <p class="top">Key Takeaways from Webinar</p>
-     <span><ol style={{width:360,textAlign:'left'}}><li>What is Full Stack Development</li>
+     <span><ol class="alignment"style={{width:360}}><li>What is Full Stack Development</li>
      <li>What is Full Stack Development</li>
      <li>Opportunities in full stack</li>
      <li>Future Scope</li>
@@ -87,12 +93,13 @@ class Class extends Component {
     </div>
     </div>
     
-    <div class="col-sm-6" ><img class="topnav2" src="guvi.png"/>
+    <div class="col-sm-6" style={{    paddingLeft: 0
+}}><img class="topnav2" src="guvi.png"/>
       <p class="alignment">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.   </p>
     </div>
     <div class="col-sm-6" >
-     <span style={{float:"left"}}>A.Mohamed Aslam</span><br/>
-       <span style={{float:"left"}}>Ceo,Mytech services Alaska</span>  
+      <p style={{float:"left",color:'black',marginBottom:0}}>A.Mohamed Aslam</p><br/>
+       <span style={{float:"left",fontSize:12}}>Ceo,Mytech services Alaska</span>  
     </div>
   
          
@@ -104,8 +111,9 @@ class Class extends Component {
     <div class="panel-heading" role="tab" id="headingThree">
       <h4 class="panel-title">
         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            <span>Webinar On Full Stack Development</span><span class="size"style={{marginLeft: 242}}>15/2/2021</span> <span style={{paddingLeft:70}}><button  type="button" class="click">Register Now</button></span><br/>
-         <span class="presenter">by Aslam Ceo,Mytech services Alaska</span><span class="size" style={{marginLeft: 290}}>03.00pm</span>
+            <span>Webinar On Full Stack Development</span><span class="size"style={{marginLeft: 242}}>15/2/2021</span><br/>
+         <div><span class="presenter">by Aslam Ceo,Mytech services Alaska</span><span class="size" style={{marginLeft: 277}}>03.00 PM</span></div>
+         <div style={{paddingLeft:700,marginTop:-30}}><button  type="button" class="click">Register Now</button></div>
         </a>
       </h4>
     </div>
@@ -113,12 +121,14 @@ class Class extends Component {
       <div class="panel-body">
 <div class="row">
     
-    <div class="col-sm-6">
+    <div class="col-sm-6"style={{
+    paddingLeft: 17
+}}>
         <div class="middle">
      <p class="top">Description</p>
      <p class="alignment">Get a deeper understanding of Client Side Programming or in other words the front end development by developing highly responsive web pages across languages.A full stack web developer is a person who can develop both client and server software. In addition to mastering HTML and CSS, he/she also knows how to: Program a browser (like using JavaScript, jQuery, Angular, or Vue) Program a server (like using PHP, ASP, Python, or Node)</p>
       <p class="top">Key Takeaways from Webinar</p>
-     <span><ol style={{width:360,textAlign:'left'}}><li>What is Full Stack Development</li>
+     <span><ol class="alignment" style={{width:360}}><li>What is Full Stack Development</li>
      <li>What is Full Stack Development</li>
      <li>Opportunities in full stack</li>
      <li>Future Scope</li>
@@ -126,12 +136,13 @@ class Class extends Component {
     </div>
     </div>
     
-    <div class="col-sm-6" ><img class="topnav2" src="guvi.png"/>
+    <div class="col-sm-6" style={{    paddingLeft: 0
+}}><img class="topnav2" src="guvi.png"/>
       <p class="alignment">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.   </p>
     </div>
     <div class="col-sm-6" >
-     <span style={{float:"left"}}>A.Mohamed Aslam</span><br/>
-       <span style={{float:"left"}}>Ceo,Mytech services Alaska</span>  
+     <p style={{float:"left",color:'black',marginBottom:0}}>A.Mohamed Aslam</p><br/>
+       <span style={{float:"left",fontSize:12}}>Ceo,Mytech services Alaska</span>  
     </div>
   
          
