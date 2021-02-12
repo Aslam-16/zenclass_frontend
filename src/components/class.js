10675198a2f11937880d768acc1e9eca98d31f1e
slamaslam466 <slamaslam466@gmail.com>
2021-02-12 12:26:39 +0530

    changes

--- src/components/class.js
+++ src/components/class.js
@@ -22,11 +22,16 @@ class Class extends Component {
   <div class="panel panel-default">
     <div class="panel-heading active" role="tab" id="headingOne">
       <h4 class="panel-title">
-        <a  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-         <span>Webinar on Full Stack Development</span><br/>
-         <span class="presenter">by Aslam,Ceo, Mytech services Alaska</span>
-         <div style={{paddingLeft:0,marginTop:-33,marginLeft: 120}}><span class="size"style={{marginLeft: 530,marginTop:-20}}>15/2/2021</span><br/><span class="size" style={{marginLeft: 530}}>03.00 PM</span>
- <div style={{paddingLeft:700,marginTop:-25}}><button  type="button" class="click">Register Now</button></div>
+        <a  style={{
+    paddingBottom: 0,
+    height: 55
+}}role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         <span>Webinar on Full Stack Development</span>
+         <p class="presenter" style={{
+    paddingTop: 5,color:"black",marginBottom: 0
+}}>by Aslam,Ceo, Mytech services Alaska</p>
+         <div style={{paddingLeft:0,marginTop:-33,marginLeft: 120}}><span class="size"style={{marginLeft: 530,marginTop:-20}}>15/2/2021</span><br/><p class="size" style={{marginLeft: 530,color:"black",paddingTop:5,width:90}}>03.00 PM</p>
+ <div style={{paddingLeft:700,marginTop:-38}}><button  type="button" class="click">Register Now</button></div>
        </div> </a>
 
       </h4>
@@ -67,10 +72,12 @@ class Class extends Component {
     <div class="panel-heading" role="tab" id="headingTwo">
       <h4 class="panel-title">
         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          <span>Webinar on Full Stack Development</span><br/>
-         <span class="presenter">by Aslam,Ceo, Mytech services Alaska</span>
-         <div style={{paddingLeft:0,marginTop:-33,marginLeft: 120}}><span class="size"style={{marginLeft: 530,marginTop:-20}}>15/2/2021</span><br/><span class="size" style={{marginLeft: 530}}>03.00 PM</span>
- <div style={{paddingLeft:700,marginTop:-25}}><button  type="button" class="click">Register Now</button></div>
+          <span>Webinar on Full Stack Development</span>
+   <p class="presenter" style={{
+    paddingTop: 5,color:"black",marginBottom: 0
+}}>by Aslam,Ceo, Mytech services Alaska</p>
+         <div style={{paddingLeft:0,marginTop:-33,marginLeft: 120}}><span class="size"style={{marginLeft: 530,marginTop:-20}}>15/2/2021</span><br/><p class="size" style={{marginLeft: 530,color:"black",paddingTop:5,width:90}}>03.00 PM</p>
+ <div style={{paddingLeft:700,marginTop:-38}}><button  type="button" class="click">Register Now</button></div>
        </div>
         </a>
       </h4>
@@ -113,11 +120,13 @@ class Class extends Component {
     <div class="panel-heading" role="tab" id="headingThree">
       <h4 class="panel-title">
         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            <span>Webinar on Full Stack Development</span><br/>
-         <div><span class="presenter">by Aslam,Ceo, Mytech services Alaska</span>
-         <div style={{paddingLeft:0,marginTop:-33,marginLeft: 120}}><span class="size"style={{marginLeft: 530,marginTop:-20}}>15/2/2021</span><br/><span class="size" style={{marginLeft: 530}}>03.00 PM</span>
- <div style={{paddingLeft:700,marginTop:-25}}><button  type="button" class="click">Register Now</button></div>
-       </div></div>
+            <span>Webinar on Full Stack Development</span>
+          <p class="presenter" style={{
+    paddingTop: 5,color:"black",marginBottom: 0
+}}>by Aslam,Ceo, Mytech services Alaska</p>
+         <div style={{paddingLeft:0,marginTop:-33,marginLeft: 120}}><span class="size"style={{marginLeft: 530,marginTop:-20}}>15/2/2021</span><br/><p class="size" style={{marginLeft: 530,color:"black",paddingTop:5,width:90}}>03.00 PM</p>
+ <div style={{paddingLeft:700,marginTop:-38}}><button  type="button" class="click">Register Now</button></div>
+       </div>
         </a>
       </h4>
     </div>
--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -2,7 +2,11 @@ function Header() {
   return (
 <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
 		<div class="container-fluid">
-      <a class="navbar-brand"style={{marginLeft:50}} href="#"><img class="logo"src="download.png"/></a>
+      <a class="navbar-brand"style={{marginLeft:50,padding:0}} href="#"><img style={{
+    width: 90,
+    paddingBottom: 20,
+    paddingTop: 10
+	  }}class="logo"src="zenClass.png"/></a>
       <span><img class="topnav1" src="guvi.png"/></span> 
 			{/* <div class="navbar-header">
 			</div> */}
